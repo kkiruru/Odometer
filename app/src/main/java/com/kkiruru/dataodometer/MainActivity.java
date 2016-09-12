5820ceff145b6bdc6adfbf8e9cf96742e325b719
허현(Hyun Hur)/Platform개발2팀/SKTECH <hyun2@sk.com>
2016-09-12 19:52:01 +0900

    [kkiruru] 1. 기본 Layout 구성

--- app/src/main/java/com/kkiruru/dataodometer/MainActivity.java
+++ app/src/main/java/com/kkiruru/dataodometer/MainActivity.java
@@ -2,6 +2,7 @@ package com.kkiruru.dataodometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void onIncrese(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.incease_1:
+                break;
+            case R.id.incease_7:
+                break;
+            case R.id.incease_13:
+                break;
+            case R.id.incease_137:
+                break;
+        }
+    }
+
+
+    public void onDecrese(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.decease_1:
+                break;
+            case R.id.decease_7:
+                break;
+            case R.id.decease_13:
+                break;
+            case R.id.decease_137:
+                break;
+        }
+    }
+
 }
